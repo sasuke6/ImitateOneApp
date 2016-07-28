e23d64453d374e587efee372bd326be51bae0172
Hsu <xusasuke6@gmail.com>
2016-07-28 20:37:39 +0800

    initial

---
+++ ImitateOneApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImitateOneApp
+//
+//  Created by ZhiyongXu on 7/28/16.
+//  Copyright © 2016 ZhiyongXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
