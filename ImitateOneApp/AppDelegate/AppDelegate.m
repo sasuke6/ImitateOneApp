3560edb77233e83ead9ec0270012eaca040179eb
Hsu <xusasuke6@gmail.com>
2016-08-05 00:03:14 +0800

    set up launch view

--- ImitateOneApp/AppDelegate/AppDelegate.m
+++ ImitateOneApp/AppDelegate/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <RDVTabBarController.h>
+#import <RDVTabBarItem.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +19,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyWindow];
+    
+    [self switchToMainView];
+    
     return YES;
 }
 
@@ -40,6 +48,37 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)switchToMainView {
+    
+    RDVTabBarController *tabbarVC = [[RDVTabBarController alloc] init];
+    
+//    BBTGirlsVC *girlVC = [[BBTGirlsVC alloc] init];
+//    UINavigationController *girlsVCNav = [[UINavigationController alloc] initWithRootViewController:girlVC];
+    
+//    BBTRankingListVC *rankingVC = [[BBTRankingListVC alloc] init];
+//    UINavigationController *rankingVCNav = [[UINavigationController alloc] initWithRootViewController:rankingVC];
+//    
+//    BBTChatListVC *chatVC = [[BBTChatListVC alloc] init];
+//    UINavigationController *chatVCNav = [[UINavigationController alloc] initWithRootViewController:chatVC];
+//    
+//    BBTMeForBoyVC *meVC = [[BBTMeForBoyVC alloc] init];
+//    UINavigationController *meVCNav = [[UINavigationController alloc] initWithRootViewController:meVC];
+    
+//    tabbarVC.viewControllers = @[girlsVCNav, rankingVCNav, chatVCNav, meVCNav];
+    
+    NSArray *titles = @[@"首页", @"阅读", @"音乐", @"电影"];
+    for (int i = 0; i < tabbarVC.viewControllers.count; i++) {
+        RDVTabBarItem *item = tabbarVC.tabBar.items[i];
+        [item setFinishedSelectedImage:[UIImage imageNamed:@"icon_test"]
+           withFinishedUnselectedImage:[UIImage imageNamed:@"icon_test_unselected"]];
+        item.title = titles[i];
+//        [self setupTabbarItem:item];
+    }
+    
+    self.window.rootViewController = tabbarVC;
+    
 }
 
 @end
