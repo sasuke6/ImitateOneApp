9109d066a764478ab886ae209e25fbb0aa078990
Hsu <xusasuke6@gmail.com>
2016-08-06 13:01:55 +0800

    tabController has complete

--- ImitateOneApp/AppDelegate/AppDelegate.m
+++ ImitateOneApp/AppDelegate/AppDelegate.m
@@ -22,67 +22,82 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    [self.window makeKeyWindow];
-    
+    [self.window makeKeyAndVisible];
     [self switchToMainView];
     
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
 }
+
+/**
+ *  ViewController
+ */
 
 - (void)switchToMainView {
     
-    RDVTabBarController *tabbarVC = [[RDVTabBarController alloc] init];
+    RDVTabBarController *tabbarVC      = [[RDVTabBarController alloc] init];
+
+    HCYHomeVC *homeVC                  = [[HCYHomeVC alloc] init];
+    UINavigationController *homeVCNav  = [[UINavigationController alloc] initWithRootViewController:homeVC];
     
-//    BBTGirlsVC *girlVC = [[BBTGirlsVC alloc] init];
-//    UINavigationController *girlsVCNav = [[UINavigationController alloc] initWithRootViewController:girlVC];
-    
-//    BBTRankingListVC *rankingVC = [[BBTRankingListVC alloc] init];
-//    UINavigationController *rankingVCNav = [[UINavigationController alloc] initWithRootViewController:rankingVC];
-//    
-//    BBTChatListVC *chatVC = [[BBTChatListVC alloc] init];
-//    UINavigationController *chatVCNav = [[UINavigationController alloc] initWithRootViewController:chatVC];
-//    
-//    BBTMeForBoyVC *meVC = [[BBTMeForBoyVC alloc] init];
-//    UINavigationController *meVCNav = [[UINavigationController alloc] initWithRootViewController:meVC];
-    
-//    tabbarVC.viewControllers = @[girlsVCNav, rankingVCNav, chatVCNav, meVCNav];
+    HCYReadVC *readVC                  = [[HCYReadVC alloc] init];
+    UINavigationController *readVCNav  = [[UINavigationController alloc] initWithRootViewController:readVC];
+
+    HCYMusicTableVC *musicVC           = [[HCYMusicTableVC alloc] init];
+    UINavigationController *musicVCNav = [[UINavigationController alloc] initWithRootViewController:musicVC];
+
+    HCYMovieTableVC *movieVC           = [[HCYMovieTableVC alloc] init];
+    UINavigationController *movieVCNav = [[UINavigationController alloc] initWithRootViewController:movieVC];
+
+    tabbarVC.viewControllers           = @[homeVCNav, readVCNav, musicVCNav, movieVCNav];
     
     NSArray *titles = @[@"首页", @"阅读", @"音乐", @"电影"];
     for (int i = 0; i < tabbarVC.viewControllers.count; i++) {
-        RDVTabBarItem *item = tabbarVC.tabBar.items[i];
-        [item setFinishedSelectedImage:[UIImage imageNamed:@"icon_test"]
-           withFinishedUnselectedImage:[UIImage imageNamed:@"icon_test_unselected"]];
-        item.title = titles[i];
-//        [self setupTabbarItem:item];
+        RDVTabBarItem *item  = tabbarVC.tabBar.items[i];
+        NSString *unselected = [NSString stringWithFormat:@"tab_default_%d",i+1];
+        NSString *selected   = [NSString stringWithFormat:@"tab_selected_%d", i+1];
+        [item setFinishedSelectedImage:[UIImage imageNamed:selected]
+           withFinishedUnselectedImage:[UIImage imageNamed:unselected]];
+        item.title           = titles[i];
+        [self setupTabbarItem:item];
     }
+    
+    RDVTabBar *tabBar = [tabbarVC tabBar];
+    [tabBar setFrame:CGRectMake(CGRectGetMinX(tabBar.frame), CGRectGetMinY(tabBar.frame), CGRectGetWidth(tabBar.frame), 60)];
+
     
     self.window.rootViewController = tabbarVC;
     
+}
+
+- (void)setupTabbarItem:(RDVTabBarItem *)tabbarItem {
+    
+    NSDictionary *unselectedTitleAttr    = @{NSForegroundColorAttributeName:[UIColor grayColor]};
+    tabbarItem.unselectedTitleAttributes = unselectedTitleAttr;
+
+    NSDictionary *selectedTitleAttr      = @{NSForegroundColorAttributeName:[UIColor blueColor]};
+    tabbarItem.selectedTitleAttributes   = selectedTitleAttr;
 }
 
 @end
