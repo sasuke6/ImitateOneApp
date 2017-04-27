0ef24127323531cd087392118b56fab254acd400
Hsu <xusasuke6@gmail.com>
2017-04-27 23:55:02 +0800

    homeview setview

--- ImitateOneApp/Common/Header/Constants.h
+++ ImitateOneApp/Common/Header/Constants.h
@@ -16,9 +16,15 @@
 #pragma mark - UIColor
 
 #define HCYViewControllerBGColor                [UIColor colorWithRed:250 / 255.0 green:252 / 255.0 blue:255 / 255.0 alpha:1]// #FAFCFF
-
-
+#define HCYShadowColor                          [UIColor colorWithWhite:102 / 255.0 alpha:1]// #666666
 // HUD
 #define HUD_DELAY                               1.5
+
+
+// 屏幕高度
+#define SCREEN_HEIGHT             [[UIScreen mainScreen] bounds].size.height
+// 屏幕宽度
+#define SCREEN_WIDTH              [[UIScreen mainScreen] bounds].size.width
+#define FULL_WIDTH                SCREEN_WIDTH
 
 #endif /* Constants_h */
--- ImitateOneApp/Feature/Home/Views/HCYHomeView.m
+++ ImitateOneApp/Feature/Home/Views/HCYHomeView.m
@@ -9,6 +9,7 @@
 #import "HCYHomeView.h"
 #import "HCYHomeItem.h"
 #import <Masonry.h>
+#import "Constants.h"
 
 NSString *const kHCYHomeViewID = @"MLBHomeViewID";
 
@@ -69,7 +70,164 @@ NSString *const kHCYHomeViewID = @"MLBHomeViewID";
 }
 
 - (void)setupViews {
+    if (_scrollView) {
+        return;
+    }
+    
+    self.backgroundColor = [UIColor clearColor];
+    
+    _scrollView = ({
+        UIScrollView *scrollView = [UIScrollView new];
+        scrollView.showsVerticalScrollIndicator = NO;
+        [scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self);
+        }];
+        scrollView;
+    
+    });
+    
+    
+    _contentView = ({
+        UIView *view = [UIView new];
+        view.backgroundColor = [UIColor whiteColor];
+        view.layer.shadowColor = HCYShadowColor.CGColor;// #666666
+        view.layer.shadowRadius = 2;
+        view.layer.shadowOffset = CGSizeZero;
+        view.layer.shadowOpacity = 0.5;
+        view.layer.cornerRadius = 5;
+        [_scrollView addSubview:view];
+        [view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(_scrollView).insets(UIEdgeInsetsMake(76, 12, 184, 12));
+            make.width.equalTo(@(SCREEN_WIDTH - 24));
+        }];
+        
+        view;
+    });
+    
+    _coverView = ({
+        UIImageView *imageView = [UIImageView new];
+        imageView.backgroundColor = [UIColor whiteColor];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        imageView.clipsToBounds = YES;
+        imageView.userInteractionEnabled = YES;
+//        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(coverTapped)];
+//        [imageView addGestureRecognizer:tap];
+        [_contentView addSubview:imageView];
+        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.top.right.equalTo(_contentView).insets(UIEdgeInsetsMake(6, 6, 0, 6));
+            make.height.equalTo(imageView.mas_width).multipliedBy(0.75);
+        }];
+        
+        imageView;
+    });
+    
+    
+    _titleLabel = ({
+        UILabel *label = [UILabel new];
+        label.backgroundColor = [UIColor whiteColor];
+//        label.textColor = MLBGrayTextColor;
+//        label.font = FontWithSize(10);
+        label.textAlignment = NSTextAlignmentRight;
+        [label setContentHuggingPriority:251 forAxis:UILayoutConstraintAxisHorizontal];
+        [_contentView addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_coverView.mas_bottom).offset(8);
+            make.left.greaterThanOrEqualTo(_volLabel.mas_right).offset(4);
+            make.right.equalTo(_coverView);
+        }];
+        
+        label;
+    });
+    
+    _contentTextView = ({
+        UITextView *textView = [UITextView new];
+        textView.backgroundColor = [UIColor whiteColor];
+//        textView.textColor = MLBLightBlackTextColor;
+//        textView.font = FontWithSize(14);
+        textView.editable = NO;
+        [_contentView addSubview:textView];
+        [textView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_volLabel.mas_bottom).offset(15);
+            make.left.right.equalTo(_coverView);
+            _textViewHeightConstraint = make.height.equalTo(@0);
+        }];
+        
+        textView;
+    });
+    
+    _dateLabel = ({
+        UILabel *label = [UILabel new];
+        label.backgroundColor = [UIColor whiteColor];
+//        label.textColor = MLBDarkGrayTextColor;
+//        label.font = FontWithSize(12);
+        [_contentView addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_contentTextView.mas_bottom).offset(10);
+            make.right.equalTo(_coverView);
+            make.bottom.equalTo(_contentView).offset(-12);
+        }];
+        
+        label;
+    });
+    
+    _locationLabel = ({
+        UILabel *label = [UILabel new];
+        label.backgroundColor = [UIColor whiteColor];
+//        label.textColor = MLBDarkGrayTextColor;
+//        label.font = FontWithSize(12);
+        [_contentView addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(_dateLabel);
+            make.right.equalTo(_dateLabel.mas_left).offset(-10);
+        }];
+        
+        label;
+    });
+    
+    _temperatureLabel = ({
+        UILabel *label = [UILabel new];
+        label.backgroundColor = [UIColor whiteColor];
+//        label.textColor = MLBDarkGrayTextColor;
+//        label.font = FontWithSize(12);
+        [_contentView addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(_dateLabel);
+            make.right.equalTo(_locationLabel.mas_left).offset(-2);
+        }];
+        
+        label;
+    });
+    
+    _weatherView = ({
+        UIImageView *imageView = [UIImageView new];
+        imageView.backgroundColor = [UIColor whiteColor];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        [_contentView addSubview:imageView];
+        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.height.equalTo(@24);
+            make.centerY.equalTo(_dateLabel);
+            make.right.equalTo(_temperatureLabel.mas_left).offset(-5);
+        }];
+        
+        imageView;
+    });
+    
     
 }
+
+#pragma mark - Action
+
+- (void)diaryButtonClicked {
+    if (_clickedButton) {
+        _clickedButton(MLBActionTypeDiary);
+    }
+}
+
+- (void)likeButtonClicked {
+    if (_clickedButton) {
+        _clickedButton(MLBActionTypePraise);
+    }
+}
+
 
 @end
